fed8b278d188a6801116d1004d1f2f303e564035
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-05-01 15:26:53 -0400

    Initialization

---
+++ app/src/main/java/com/example/whowantstobeamillionaredjb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.whowantstobeamillionaredjb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Splash Screen Activity
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
