2e29306c8c671ff4b9783b7af7e7df20f34dc280
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-05-01 15:35:51 -0400

    Activity Creation
    Created empty activities for all screens in the game.

---
+++ app/src/main/java/com/example/whowantstobeamillionaredjb/Question7.java
@@ -0,0 +1,14 @@
+package com.example.whowantstobeamillionaredjb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Question7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question7);
+    }
+}
