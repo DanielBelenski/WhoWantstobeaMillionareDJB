d83d9ccc30f1247b5bafda52759a370fb69d62c7
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-05-01 16:13:06 -0400

    Design
    Changed the icon and designed the splash screen.

--- app/src/main/java/com/example/whowantstobeamillionaredjb/MainActivity.java
+++ app/src/main/java/com/example/whowantstobeamillionaredjb/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.whowantstobeamillionaredjb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 //Splash Screen Activity
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toGame(View view){
+
     }
 }
